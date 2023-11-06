fdc27973615d51051b7955efb079751dcff5d80b
PhamDuy <vanduy342001@gmail.com>
2023-11-06 21:05:19 +0700

    dynamic links

--- store/models.py
+++ store/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 
 # Create your models here.
@@ -34,6 +35,9 @@ class Product(models.Model):
     class Meta:
         verbose_name_plural = "Products"
         ordering = ("-created",)
+
+    def get_absolute_url(self):
+        return reverse("store:products_detail", args=[self.slug])
 
     def __str__(self) -> str:
         return self.title
